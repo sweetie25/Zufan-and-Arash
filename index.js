0d8f4de878388b9a6c34a376d745a295ec812e2d
Zufan E <99605646+sweetie25@users.noreply.github.com>
2023-01-31 01:17:37 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-// !! IMPORTANT README:
-
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
 "use strict";
+const projectTile = document.querySelectorAll(".project-tile");
+
+for (var y = 0; y < projectTile.length; y++) {
+    projectTile[y].addEventListener("mouseover", codeHover);
+    projectTile[y].addEventListener("mouseout", codeOut);
+}
+
+function codeHover() {
+    let code = document.querySelectorAll(".code");
+
+    for (var i = 0; i < code.length; i++) {
+        code[i].style.color = "orange";
+        code[i].style.transition = "0.3s";
+    }
+}
+
+function codeOut() {
+    let code = document.querySelectorAll(".code");
+
+    for (var i = 0; i < code.length; i++) {
+        code[i].style.color = "#303841";
+    }
+}
